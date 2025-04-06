651afec7c2616c24f4c00ecca77059ce76c8fdf9
Steven <st.latuszek@gmail.com>
2025-04-06 14:53:38 +0200

    desable qmlformat

--- config/plugins/lang/qml.nix
+++ config/plugins/lang/qml.nix
@@ -1,20 +1,20 @@
 { pkgs, ... }:
 {
   plugins = {
-    conform-nvim.settings = {
-      formatters = {
-        qmlformat = {
-          command = "${pkgs.kdePackages.qtdeclarative}/bin/qmlformat";
-          args = [
-            "-w"
-            "2"
-            "$FILENAME"
-          ];
-        };
-      };
-
-      formatters_by_ft.qml = [ "qmlformat" ];
-    };
+    # conform-nvim.settings = {
+    #   formatters = {
+    #     qmlformat = {
+    #       command = "${pkgs.kdePackages.qtdeclarative}/bin/qmlformat";
+    #       args = [
+    #         "-w"
+    #         "2"
+    #         "$FILENAME"
+    #       ];
+    #     };
+    #   };
+    #
+    #   formatters_by_ft.qml = [ "qmlformat" ];
+    # };
 
     lsp.servers.qmlls = {
       enable = true;
